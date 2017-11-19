661b321733346d2e647237b02c5c823ceb6fb760
Felipe Sena <felipedepaulasena@hotmail.com>
2017-11-19 11:15:08 -0200

    Remoção de import desnecessário

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
 
 
 def generategraph():
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import artistlisteners, userartists, graph
-import sys
+
 
 
 def main():
